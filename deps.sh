6fc0b5aecd974ff521a337316f0a6fa2920d919b
Kevin Wang <kevin@xorkevin.com>
2019-05-28 20:50:08 -0700

    Remove mathjax

--- deps.sh
+++ deps.sh
@@ -8,7 +8,3 @@ mkdir -p $output
 mkdir -p $output/css
 cp node_modules/@fortawesome/fontawesome-free/css/all.min.css $output/css
 cp -r node_modules/@fortawesome/fontawesome-free/webfonts $output
-
-output=$vendorpath/mathjax
-mkdir -p $output
-cp node_modules/mathjax/config/TeX-AMS_CHTML-full.js $output/mathjax.js
