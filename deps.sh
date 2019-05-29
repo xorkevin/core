71718dbaf382ba0de32486406a91ec2856a50826
Kevin Wang <kevin@xorkevin.com>
2019-05-28 18:37:21 -0700

    Fix mathjax dependencies

--- deps.sh
+++ deps.sh
@@ -12,3 +12,11 @@ cp -r node_modules/@fortawesome/fontawesome-free/webfonts $output
 output=$vendorpath/mathjax
 mkdir -p $output
 cp node_modules/mathjax/MathJax.js $output/mathjax.js
+mkdir -p $output/jax/input
+cp -r node_modules/mathjax/jax/input/TeX $output/jax/input
+mkdir -p $output/jax/output
+cp -r node_modules/mathjax/jax/output/CommonHTML $output/jax/output
+mkdir -p $output/extensions
+cp node_modules/mathjax/extensions/tex2jax.js $output/extensions
+mkdir -p $output/extensions/TeX
+cp node_modules/mathjax/extensions/TeX/{AMSmath.js,AMSsymbols.js,noErrors.js,noUndefined.js} $output/extensions/TeX
