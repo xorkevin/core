489d89ba9a6a50f515425864f6bd4e8e584c287a
Kevin Wang <kevin@xorkevin.com>
2020-01-13 15:37:04 -0800

    Add inter-ui and merriweather font deps

--- deps.sh
+++ deps.sh
@@ -6,5 +6,15 @@ mkdir -p $vendorpath
 output=$vendorpath/fontawesome
 mkdir -p $output
 mkdir -p $output/css
-cp node_modules/@fortawesome/fontawesome-free/css/all.min.css $output/css
-cp -r node_modules/@fortawesome/fontawesome-free/webfonts $output
+cp node_modules/fork-awesome/css/fork-awesome.min.css $output/css
+cp -r node_modules/fork-awesome/fonts $output
+
+output=$vendorpath/inter-ui
+mkdir -p $output
+cp node_modules/inter-ui/inter.css $output
+cp -r 'node_modules/inter-ui/Inter (web)' $output
+
+output=$vendorpath/merriweather
+mkdir -p $output
+cp node_modules/typeface-merriweather/index.css $output
+cp -r node_modules/typeface-merriweather/files $output
